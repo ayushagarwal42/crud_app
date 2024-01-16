45ddba5ee1f5f0a79ca491490a9e2e7775a6acdf
Ayush Agarwal <ayushagarwalds12@gmail.com>
2024-01-16 19:23:19 +0530

    changes

--- routes/routes.js
+++ routes/routes.js
@@ -1,24 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/users");
-const multer = require("multer");
+// const multer = require("multer");
+const upload=require('../multer/multer');
 const fs=require('fs');
 
-//image upload
-var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "./uploads");
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
-    },
-});
 
-var upload = multer({
-    storage: storage,
-}).single("image");
 
-// insert an usetr into database route
+// insert an user into database route
 router.post("/add", upload, async(req, res) => {
     try{
         const user = new User({
@@ -41,23 +30,10 @@ router.post("/add", upload, async(req, res) => {
             type: "danger",
         });
     }  
-    // user.save((err)=>{
-    //     if(err){
-    //         console.log(err);
-    //         res.json({message:err.message,type:'danger'});
-    //     }else{
-    //         req.session.message={
-    //             type:'success',
-    //             message:'User added successfully'
-    //         };
-    //         res.redirect('/');
-    //     }
-    // });
 });
 
 //get all users route
 router.get("/", async(req, res) => {
-    // res.render('index', { title: 'HomePage' });
     try {
         const users = await User.find().exec();
         res.render("index", {
@@ -67,26 +43,6 @@ router.get("/", async(req, res) => {
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
-    // User.find().exec()
-    //     .then(users => {
-    //         res.render("index", {
-    //             title: "HomePage",
-    //             users: users,
-    //         });
-    //     })
-    //     .catch(err => {
-    //         res.status(500).json({ error: err.message });
-    //     });
-    // User.find().exec((err, users) => {
-    //     if (err) {
-    //         res.json({ message: err.message });
-    //     } else {
-    //         res.render("index", {
-    //             title: "HomePage",
-    //             users: users,
-    //         });
-    //     }
-    // });
 });
 
 router.get("/add", (req, res) => {
@@ -152,7 +108,5 @@ router.post('/update/:id',upload,async(req,res)=>{
         res.redirect('/');
     }
 });
-
-// router.post("update/:id")
 
 module.exports = router;
